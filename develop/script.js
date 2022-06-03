3e204c6690ea1f44333d0d1edd387c5ea8d0beb9
Micah <micahculverhouse1@gmail.com>
2022-06-02 19:08:25 -0600

    script test

---
+++ develop/script.js
@@ -0,0 +1,8 @@
+var btnOne = $("#btnStart1")
+var btnTwo = $("#btnStart2")
+var btnThree =$("#btnsStart3")
+var btnFour =$("#btnStart4")
+
+$("#btnStart1").click(function(){
+    window.location.replace("citys.html")
+});
