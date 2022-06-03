1fd5fe5949191c5caad13cfd079bc989578c8474
Micah <micahculverhouse1@gmail.com>
2022-06-02 21:23:16 -0600

    CHECKED BOXES LETS GO

--- develop/script.js
+++ develop/script.js
@@ -4,49 +4,29 @@ $("#btnStart1").click(function(){
 });
 
 $("#btnStart2").click(function(){
-
     window.location = "ingredients.html"
 });
 
 $("#btnStart3").click(function(){
     window.location = "results.html"
+    onClickBox();
 });
 
 $("#btnStart4").click(function(){
     window.location = "index.html"
 });
 
-let boxes = $(".box").length;
 
-function save() {	
-    for(let i = 1; i <= boxes; i++){
-        var checkbox = document.getElementById(String(i));
-      localStorage.setItem("checkbox" + String(i), checkbox.checked);	
+
+
+function onClickBox() {
+    // var checked = [($("#box-1"),$("#box-2"),$("#box-3"))];
+    if($('#box-1').is(':checked')){
+        alert('is checked!');
     }
+    console.log(localStorage);
   }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let cityArray = ["Seattle,Phoenix,Honolulu,Boise,Las Vegas,Portland,Houston,Los Angeles,DC"]
-
-let ingredientsArray = ["Eggs,Bread,Milk,Steak,Orange Juice,Rice,Butter"]
 
 
