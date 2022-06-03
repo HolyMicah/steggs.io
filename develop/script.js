38fb2b24df7d29ff2056fed64f9a645d2891c209
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-06-02 19:18:00 -0700

    City Array

--- develop/script.js
+++ develop/script.js
@@ -6,3 +6,25 @@ var btnFour =$("#btnStart4")
 $("#btnStart1").click(function(){
     window.location.replace("citys.html")
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let cityArray = [Seattle,Phoenix,Honolulu,Boise,Las Vegas,Portland,Houston,Los Angeles,DC]
