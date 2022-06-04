7b489d72c47b21e28a01c2f174451d646a720e1a
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-06-04 11:56:11 -0700

    added to to JS Function call

--- develop/script.js
+++ develop/script.js
@@ -96,6 +96,23 @@ function onClickBox() {
     }
   }
 
-
+  function getInfoFromKroger(){
+    $.ajax({
+        url: 'https://url.com',
+        type: 'post',
+        data: {
+            access_token: 'XXXXXXXXXXXXXXXXXXX'
+        },
+        headers: {
+            Header_Name_One: 'Header Value One',   //If your header name has spaces or any other char not appropriate
+            "Header Name Two": 'Header Value Two'  //for object property name, use quoted notation shown in second
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.info(data);
+        }
+    }
+    )
+    };
 
 
