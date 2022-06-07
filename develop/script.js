797f92806c13d4087a7394ef435440149bd7522c
Micah <micahculverhouse1@gmail.com>
2022-06-07 16:35:28 -0600

    page removal

--- develop/script.js
+++ develop/script.js
@@ -6,28 +6,21 @@ $("#btnStart1").click(function(){
 
 
 $("#btnStart2").click(function(){
-   pageChange(); 
-    
-});
-
-
-$("#btnStart4").click(function(){
-    window.location = "index.html"
-    
-});
-
-
-$("#submitIpt").click(function(){
   apiCall();
-  // localInput();
+    
 });
+
+
+
+
+
 
 
 // function localInput(){
 //   var listItem = document.createElement('li');
-//   let zip = localStorage.getItem("input");
-//   listItem.textContent = zip
-// }
+//   var zip = localStorage.getItem("input");
+//   $(listItem).append(zip);
+// };
 
 
 
@@ -37,17 +30,19 @@ $("#submitIpt").click(function(){
 
 
 function apiCall(){
-    let searchIpt = $(".searchBar").val();
-    localStorage.setItem("input", searchIpt);
+  
+  var searchIpt = $(".searchBar").val();
+  localStorage.setItem("input", searchIpt);
+
 
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.kroger.com/v1/locations?filter.zipCode.near=" + searchIpt,
+  "url": "https://api.kroger.com/v1/locations?filter.zipCode.near=" + searchIpt, 
   "method": "GET",
   "headers": {
     "Accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCIsImV4cCI6MTY1NDY0MDE0MiwiaWF0IjoxNjU0NjM4MzM3LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU4MTc4NjNhLTUzMTYtNThkYi1iNmY1LTBjYjI1OTVlNTQxMCIsInNjb3BlIjoiIiwiYXV0aEF0IjoxNjU0NjM4MzQyOTcwOTM2MDQzLCJhenAiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCJ9.AJGea4od9zO6fccybg3AoBfkzRq_FnOutB74JYOOLeqCUQyebX2NH2P7znZ7JThTp3yafzAf07JIJEPJoYcasPEE5JuSD1sh8mtCIR70pInbq-XNq0PHloC8ecTeg2znBCNEkPIjJ1DpAJjrn80K_Aa2DxtH4_y_oWNW3YAkGOEXl-Fsu5ybHAKYKXE6EBQ4L44_omitC1taHFD4wAzeXNmqP2n9PbXofZjqTnua6gAk9l8RtT2YB72j0oJtGRSJtHa_bV2MPNqHLGK8WI_jn8602L2-BumDW6cwIP5oJUePDfe9XrQwmy8QZVQIJzsgwuv3c50t_3VdwIf8TvcMrA"
+    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCIsImV4cCI6MTY1NDY0MTM4OSwiaWF0IjoxNjU0NjM5NTg0LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU4MTc4NjNhLTUzMTYtNThkYi1iNmY1LTBjYjI1OTVlNTQxMCIsInNjb3BlIjoiIiwiYXV0aEF0IjoxNjU0NjM5NTg5MzcwMTczOTEwLCJhenAiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCJ9.ZacTlW0QA9lbSiMzm2h-GUvMAmRMY8aDYzVsXKtIJXtvAWMrHJ6Mgcvm3HXIsWrOejkfzjj1c4LOIf_pPcmTiEp3CEiyknKSWSNMS20vMFc4K36D3c8upiglWYRKTNAqUWZGKt-2do4x7xpaFXTU8Fcs1XRRytGA-EJ67n4P4RGQ6onqylgLJH64MoW2EOit3mnQrghK6PUaXqFezcD1LZqr9teMhVFWcm2v4fG3XbCdI1GyA7jBuvqK_TWBWmN0EOU3n2ZZMmliNEOCIy6tq1vaDxfeYmLDutQZ2bp2_FTTVW9P9HGlYqrK8YJ622OLpNnOdlqKOVOYzVpE7leBBw"
   }
 };
 
@@ -57,9 +52,6 @@ $.ajax(settings).done(function (response) {
 };
 
 
-function pageChange(){
-  window.location = "results.html";
-};
 
 
 
