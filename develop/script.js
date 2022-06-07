c96c593610f6cf71bd3aaa51c1068338667bdbc2
Micah <micahculverhouse1@gmail.com>
2022-06-07 15:21:38 -0600

    api and navigation fixed

--- develop/script.js
+++ develop/script.js
@@ -6,28 +6,8 @@ $("#btnStart1").click(function(){
 
 
 $("#btnStart2").click(function(){
+   pageChange(); 
     
-    let searchIpt = $(".searchBar").val();
-
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://api.kroger.com/v1/locations?filter.zipCode.near=" + searchIpt,
-      "method": "GET",
-      "headers": {
-        "Accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCIsImV4cCI6MTY1NDU3NTA5NCwiaWF0IjoxNjU0NTczMjg5LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU4MTc4NjNhLTUzMTYtNThkYi1iNmY1LTBjYjI1OTVlNTQxMCIsInNjb3BlIjoiIiwiYXV0aEF0IjoxNjU0NTczMjk0OTM1NDcwNzg5LCJhenAiOiJzdGVnZ3Npby04MjhmZmFkM2UzOWNhZGVmNThhNTdlYWRhMWJkM2UwMDU4NDI3NjQ0MzcyNDI3ODUyMCJ9.XbNJciN_QcTQmhnO_xQPMBf5XOJVxbzh_DlKN5j_7Xc59Ag4A0t0GD6RY5LnVRqKRNkeagHQJnRwytUnbjQ6tiZ_ZJlbPpb8ozxeEAW4vlCEUj4p3TL170Cs7db-P2hS0KOQl2XB7p_3X_Y-W1yMINaieOYHU3q6O_kEwhY4ek5etqXAc3RH_dcKOw0tnicacVHS3bnuvY-cxBivP0DdgUNYWpDp87Xk11gkLZP20qvjSralpdGiLeMaBLmsEHUDR0PzC7qXqXpTCKF8iaXdsE6ToWNOV3lVSHSyH1ML9Gc_YvQl2Z6OQbLLDsXTA2g6SWQ7kWZsO4wlxnJLGxCDKw"
-      }
-    };
-    
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
-
-
-
-    localStorage.setItem("input", searchIpt);  
-    // window.location = "results.html";  
 });
 
 
@@ -35,9 +15,6 @@ $("#btnStart4").click(function(){
     window.location = "index.html"
 });
 
-// var settings = [];
-// settings.push(storeDisplay);
-// I made this as an attempt to push the store loaction response to the loccationDisplay element in the HTML
 
 $("#submitIpt").click(function(){
   apiCall();
@@ -71,9 +48,7 @@ var settings = {
 };
 
 $.ajax(settings).done(function (response) {
-  console.log(response);
-   localStorage.setItem("input", searchIpt);  
-    // window.location = "results.html";  
+  console.log(response); 
 });
 };
 
